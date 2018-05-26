b6b2364c93db9ad6e2a1f5e19dafbd4c6d8a29dd
Daniel TAYLOR <danieltaylor@Daniels-MBP.home>
2018-05-26 17:17:16 +0100

    add view switch

--- app/src/main/java/com/example/danieltaylor/androidgame/CharacterSelectionActivity.java
+++ app/src/main/java/com/example/danieltaylor/androidgame/CharacterSelectionActivity.java
@@ -5,8 +5,9 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
-public class CharacterSelectionActivity extends AppCompatActivity {
+public class CharacterSelectionActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +28,16 @@ public class CharacterSelectionActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void onClick(View v){
+
+        switch (v.getId()) {
+            case R.id.character1:
+            case R.id.character2:
+            case R.id.character3:
+            case R.id.character4:
+        }
     }
 }
